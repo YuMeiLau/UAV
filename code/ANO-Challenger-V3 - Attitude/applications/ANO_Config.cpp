6958853cc98d429433326a8b0300ba6923e09165
YuMeiLau <liuyuwei_@hotmail.com>
2015-09-09 14:30:31 +0800

    Filter & Attitude estimation added.

---
+++ code/ANO-Challenger-V3 - Attitude/applications/ANO_Config.cpp
@@ -0,0 +1,51 @@
+/******************** (C) COPYRIGHT 2014 ANO Tech ***************************
+ * 作者		 ：秋雨魂
+ * 文件名  ：ANO_Config.cpp
+ * 描述    ：配置文件
+ * 官网    ：www.anotc.com
+ * 淘宝    ：anotc.taobao.com
+ * 技术Q群 ：190169595
+**********************************************************************************/
+
+#include "ANO_Config.h"
+
+ANO_Config ano;
+
+
+ANO_Config::ANO_Config(void)
+{
+	f.ANGLE_MODE = 1;
+}
+
+//指示灯
+void ANO_Config::Pilot_Light(void)
+{
+	static u8 cnt = 0,cnt2 = 0;
+	
+	
+	if(f.ARMED)
+	{
+		cnt++;
+		switch(cnt)
+		{
+			case 1:
+				led.ON1();
+				break;
+			case 20:
+				led.OFF1();
+				break;
+			case 40:
+				cnt = 0;
+				break;			
+		}
+	}
+	else
+	{
+		led.ON1(); 
+		led.ON2(); 
+	}
+	
+}
+
+
+/******************* (C) COPYRIGHT 2014 ANO TECH *****END OF FILE************/
---
+++ code/ANO-Challenger-V3 - Attitude/applications/ANO_FlyControl.cpp
@@ -0,0 +1,78 @@
+/******************** (C) COPYRIGHT 2014 ANO Tech ***************************
+ * 作者		 ：秋雨魂
+ * 文件名  ：ANO_FlyControl.cpp
+ * 描述    ：飞行控制
+ * 官网    ：www.anotc.com
+ * 淘宝    ：anotc.taobao.com
+ * 技术Q群 ：190169595
+**********************************************************************************/
+#include "ANO_FlyControl.h"
+
+ANO_FlyControl fc;
+
+
+ANO_FlyControl::ANO_FlyControl()
+{
+	yawRate = 50;
+	//重置PID参数
+	PID_Reset();
+}
+
+//重置PID参数
+void ANO_FlyControl::PID_Reset(void)
+{
+	pid[PIDROLL].set_pid(0.15, 0.15, 0.02, 200);
+	pid[PIDPITCH].set_pid(0.15, 0.15, 0.02, 200);
+	pid[PIDYAW].set_pid(0.8, 0.45, 0, 200);
+	pid[PIDANGLE].set_pid(5, 0, 0, 0);
+	pid[PIDMAG].set_pid(2, 0, 0, 0);
+}
+
+//飞行器姿态外环控制
+void ANO_FlyControl::Attitude_Outter_Loop(void)
+{
+	int32_t	errorAngle[2];
+	Vector3f Gyro_ADC;
+	
+	//计算角度误差值
+	errorAngle[ROLL] = constrain_int32((rc.Command[ROLL] * 2) , -((int)FLYANGLE_MAX), +FLYANGLE_MAX) - imu.angle.x * 10; 
+	errorAngle[PITCH] = constrain_int32((rc.Command[PITCH] * 2) , -((int)FLYANGLE_MAX), +FLYANGLE_MAX) - imu.angle.y * 10; 
+	errorAngle[ROLL] = applyDeadband(errorAngle[ROLL], 2); 
+	errorAngle[PITCH] = applyDeadband(errorAngle[PITCH], 2); 
+	
+	//获取角速度
+	Gyro_ADC = imu.Gyro_lpf / 4.0f;
+	
+	//得到外环PID输出
+	RateError[ROLL] = pid[PIDANGLE].get_p(errorAngle[ROLL]) - Gyro_ADC.x;
+	RateError[PITCH] = pid[PIDANGLE].get_p(errorAngle[PITCH]) - Gyro_ADC.y;
+	RateError[YAW] = ((int32_t)(yawRate) * rc.Command[YAW]) / 32 - Gyro_ADC.z;		
+}
+
+//飞行器姿态内环控制
+void ANO_FlyControl::Attitude_Inner_Loop(void)
+{
+	int32_t PIDTerm[3];
+	float tiltAngle = constrain_float( max(abs(imu.angle.x), abs(imu.angle.y)), 0 ,20);
+	
+	for(u8 i=0; i<3;i++)
+	{
+		//当油门低于检查值时积分清零
+		if ((rc.rawData[THROTTLE]) < RC_MINCHECK)	
+			pid[i].reset_I();
+		
+		//得到内环PID输出
+		PIDTerm[i] = pid[i].get_pid(RateError[i], PID_INNER_LOOP_TIME*1e-6);
+	}
+	
+	PIDTerm[YAW] = -constrain_int32(PIDTerm[YAW], -300 - abs(rc.Command[YAW]), +300 + abs(rc.Command[YAW]));	
+		
+	//油门倾斜补偿
+	rc.Command[THROTTLE] = (rc.Command[THROTTLE] - 1000) / cosf(radians(tiltAngle)) + 1000;
+	
+	//PID输出转为电机控制量
+	motor.writeMotor(rc.Command[THROTTLE], PIDTerm[ROLL], PIDTerm[PITCH], PIDTerm[YAW]);
+}	
+
+
+/************************ (C) COPYRIGHT 2014 ANO TECH *****END OF FILE**********************/
